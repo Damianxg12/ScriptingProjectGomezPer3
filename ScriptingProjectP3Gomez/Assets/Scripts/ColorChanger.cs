281446e74f6252721d12e53d9f77f1840b0993c8
Damianxg12 <715860@student.dusd.net>
2023-10-19 11:23:11 -0700

    Scripting assighment
    created scene and completed my 1st scripting assighnment

---
+++ ScriptingProjectP3Gomez/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(keycode.r))   
+    }
+}GetComponent>().material.color=Color.red
+
+
+if(Input.GetKeyDown(Keycode.G))
+
+GetComponent>()Material.color=Color.green
+
+    if(Input.GetKeyDown(KeyCode.b))
+
+    GetComponent<Renderer>().material.color=Color.blue
